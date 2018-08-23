e891855651af87e745ae853452c0d6f0e3bb5e1e
Kelly <kellygrc20@gmail.com>
2018-08-22 23:29:46 -0500

    moviendo la vista principal

--- src/js/vista-splash.js
+++ src/js/vista-splash.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
     setTimeout(function() {
-      window.location.assign('./html/principal.html');
+      window.location.assign('principal.html');
     }, 2000);
   });
